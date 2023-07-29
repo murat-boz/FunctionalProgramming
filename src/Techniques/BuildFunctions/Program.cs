2774ddee9270a9bad6934e74952ba10a3fc5db76
murat-boz <muratboz@hotmail.com>
2023-07-29 17:34:01 +0300

    Added elegant way.

--- src/Techniques/BuildFunctions/Program.cs
+++ src/Techniques/BuildFunctions/Program.cs
@@ -4,23 +4,42 @@ builder.Services.AddSingleton<Graph>();
 
 var app = builder.Build();
 
-app.MapGet("/", (int? x, int? y, int? z, Graph g) =>
+//'e' => x => v => v.X == x
+//'l' => x => v => v.X < x
+//'g' => x => v => v.X > x
+
+var createXCondition = CreateCondition(v => v.x);
+var createYCondition = CreateCondition(v => v.y);
+var createZCondition = CreateCondition(v => v.z);
+
+Func<string, Func<Vector3d, bool>> CreateCondition(Func<Vector3d, int> coordSelector)
+{
+    return val => val switch
+    {
+        ['e', .. var rest] => v => coordSelector(v) == int.Parse(rest),
+        ['l', .. var rest] => v => coordSelector(v) < int.Parse(rest),
+        ['g', .. var rest] => v => coordSelector(v) > int.Parse(rest),
+    };
+};
+
+
+app.MapGet("/", (string? x, string? y, string? z, Graph g) =>
 {
     IEnumerable<Vector3d> query = g;
 
     if (x != null)
     {
-        query = query.Where(v => v.x == x);
+        query = query.Where(createXCondition(x));
     }
 
     if (y != null)
     {
-        query = query.Where(v => v.y == y);
+        query = query.Where(createXCondition(y));
     }
 
     if (z != null)
     {
-        query = query.Where(v => v.z == z);
+        query = query.Where(createXCondition(z));
     }
 
     return query;
